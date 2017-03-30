34b9580a3f69b281c5d1cbf3d805771d92786bde
vmohan9 <vahinimohan03@gmail.com>
2017-03-30 15:42:42 -0700

    Pushing the changes made for the comments

--- glimpse/src/main/java/edu/asu/diging/glimpse/service/FileService.java
+++ glimpse/src/main/java/edu/asu/diging/glimpse/service/FileService.java
@@ -1,21 +1,32 @@
 package edu.asu.diging.glimpse.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
- 
+
+import edu.asu.diging.glimpse.web.UploadController;
+
 import java.io.File;
 import java.io.IOException;
  
+@Configuration
+@PropertySource("classpath:config.properties")
 @Service("fileService")
-public class FileService {
+public class FileService implements FileInterface {
  
-    public static final String SAVE_LOCATION = "C:/SpringUpload/";
- 
+    //public static final String SAVE_LOCATION = "C:/SpringUpload/";
+    @Value("${file_location}")
+    private String location;
+    
+    private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
     public boolean saveFile(MultipartFile multipartFile){
         boolean result = false;
         //set the saved location and create a directory location
         String fileName  = multipartFile.getOriginalFilename();
-        String location = SAVE_LOCATION;
         File pathFile = new File(location);
         //check if directory exist, if not, create directory
         if(!pathFile.exists()){
@@ -29,7 +40,7 @@ public class FileService {
             multipartFile.transferTo(pathFile);
             result = true;
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error("Unable to upload"," ", e);
         }
         return result;
     }
--- glimpse/src/main/java/edu/asu/diging/glimpse/web/UploadController.java
+++ glimpse/src/main/java/edu/asu/diging/glimpse/web/UploadController.java
@@ -1,30 +1,24 @@
 package edu.asu.diging.glimpse.web;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.util.HashMap;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
-
 import edu.asu.diging.glimpse.service.FileService;
 
 @Controller
 public class UploadController {
 	
-		@Autowired
-    	private FileService fileService;
+        @Autowired
+        private FileService fileService;
 	
 		@RequestMapping(value="/upload")
-		public String UploadCon(){
+		public String Upload(){
 			return "Upload";
 		}
 		
